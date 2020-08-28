24a2690c45c25bc025635cd1d810e35b7c91aa84
SUN <SUN@itcast.cn>
2020-08-28 12:04:38 +0800

    统一处理异常

---
+++ health_interface/src/main/java/com/itheima/exception/BusinessRuntimeException.java
@@ -0,0 +1,12 @@
+package com.itheima.exception;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ * @Version 1.0
+ */
+public class BusinessRuntimeException extends RuntimeException {
+    public BusinessRuntimeException(String message){
+        super(message);
+    }
+}
